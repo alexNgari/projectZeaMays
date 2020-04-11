6bda97015021d4c4ca8ed6a3ff99cfc74d1405cd
Alex Ngari <alex.ngari03@gmail.com>
2020-04-11 07:59:02 +0300

    Made a tweaks to support my transfer learning arch

--- src/preprocessing/image_gen.py
+++ src/preprocessing/image_gen.py
@@ -90,3 +90,34 @@ class BalanceImageGenerator():
             .batch(self.batch_size)\
             .prefetch(AUTOTUNE)
         return resampled_ds
+
+
+class MultiTaskImageGen(ImageGenerator):
+    """
+    Generator to produce batches of augmented data, modified to work for multi-task learning
+    """
+    def __init__(self, data_dir, image_size, class_labels: list, *, for_cnn=True):
+        """
+        params: directory to images belonging to one class (str)
+                integer describing number of pixels on one side of images.
+        """
+        self.for_cnn = for_cnn
+        self.data_dir = data_dir
+        self.label = data_dir.split('/')[-1]
+        self.image_size = image_size
+        self.class_labels: tf.Tensor = tf.convert_to_tensor(class_labels)
+        self.indices = tf.range(self.class_labels.shape[0])
+        self.full_set = None
+
+    def encode_labels(self, img):
+        """
+        params: encoded image and string label
+        return: image and one-hot-encoded label
+        """
+        encoded_label = tf.reshape(tf.where(self.class_labels==self.label), (1,))[0]
+        encoded_label = tf.one_hot(self.indices, self.indices.shape[0])[encoded_label]
+        faw = encoded_label[0]
+        zinc = encoded_label[1]
+        nlb = encoded_label[2]
+        # encoded_label = tf.convert_to_tensor(encoded_label, dtype=tf.int64)
+        return img, (faw, zinc)
